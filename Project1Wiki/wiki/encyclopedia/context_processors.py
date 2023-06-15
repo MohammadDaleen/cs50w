172582c1ee8ab106e37a36acad6ff259d9ebd72d
MohammadDaleen <mohammaddalien2003@gmail.com>
2023-06-15 16:43:41 +0300

    Add datalist to search bar using context processor

---
+++ Project1Wiki/wiki/encyclopedia/context_processors.py
@@ -0,0 +1,10 @@
+from . import util
+
+#  Create a context processor (i.e., a function that runs before every template is rendered and add context variables to the template context.)
+def render(request):
+    # Get all entries titles from .md files
+    entriesTitles = util.list_entries()
+    
+    # return entriesTitles to rendered templates
+    return({"entriesTitles": entriesTitles})
+    
--- Project1Wiki/wiki/wiki/settings.py
+++ Project1Wiki/wiki/wiki/settings.py
@@ -63,6 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Add a context processor (context_processors.py) to pass entriesTitles to layout templet
+                'encyclopedia.context_processors.render',
             ],
         },
     },
