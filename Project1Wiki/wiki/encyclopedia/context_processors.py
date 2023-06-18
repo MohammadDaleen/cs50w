3c83731df49552a94972baeb8f87f45dcdfd817e
Mohammad Daleen <mohammaddalien2003@gmail.com>
2023-06-18 13:42:02 +0300

    Style (py) + Organize & delete unnecessary (html)

--- Project1Wiki/wiki/encyclopedia/context_processors.py
+++ Project1Wiki/wiki/encyclopedia/context_processors.py
@@ -1,10 +1,15 @@
 from . import util
 
-#  Create a context processor (i.e., a function that runs before every template is rendered and add context variables to the template context.)
+'''
+Create a context processor (i.e., a function that runs before every template
+is rendered and add context variables to the template context.)
+'''
+
+
 def render(request):
     # Get all entries titles from .md files
     entriesTitles = util.list_entries()
     
     # return entriesTitles to rendered templates
-    return({"entriesTitles": entriesTitles})
+    return ({"entriesTitles": entriesTitles})
     
