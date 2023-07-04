511e6030ce58ce325a0e733ce3f9a32b332ed475
Mohammad Daleen <mohammaddalien2003@gmail.com>
2023-07-04 11:01:33 +0300

    Organize: urls.py

--- Project2Commerce/commerce/auctions/urls.py
+++ Project2Commerce/commerce/auctions/urls.py
@@ -5,17 +5,16 @@ from . import views
 app_name = "commerce"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("addBid", views.addBid, name="addBid"),
+    path("addComment", views.addComment, name="addComment"),
+    path("categories", views.categories, name="categories"),
+    path("category/<str:key>", views.category, name="category"),
+    path("closeAuction", views.closeAuction, name="closeAuction"),
+    path("listing/<str:id>", views.listing, name="listing"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
     path("newListing", views.newListing, name="newListing"),
-    path("listing/<str:id>", views.listing, name="listing"),
-    path("watchlist", views.watchlist, name="watchlist"),
+    path("register", views.register, name="register"),
     path("removeWatchlist", views.removeWatchlist, name="removeWatchlist"),
-    path("addBid", views.addBid, name="addBid"),
-    path("closeAuction", views.closeAuction, name="closeAuction"),
-    path("addComment", views.addComment, name="addComment"),
-    path("watchlist", views.watchlist, name="watchlist"),
-    path("categories", views.categories, name="categories"),
-    path("category/<str:key>", views.category, name="category")
+    path("watchlist", views.watchlist, name="watchlist")
 ]
