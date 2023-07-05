84eb07d5d5af482c55643de976a12497d48caea6
Mohammad Daleen <mohammaddalien2003@gmail.com>
2023-07-05 15:27:59 +0300

    Organize: finish the rest of files

--- Project2Commerce/commerce/auctions/models.py
+++ Project2Commerce/commerce/auctions/models.py
@@ -5,9 +5,7 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-# auction listings model
-    # specify a title for the listing, a text-based description, and what the starting bid should be.
-    # optionally be able to provide a URL for an image for the listing and/or a category (e.g. Fashion, Toys, Electronics, Home, etc.).
+# Auction/Listing model
 class Listing(models.Model):
     # Define CATEGORIES that will be used as choices for category column
     CATEGORIES = [("fshn", "Fashion"), 
@@ -27,6 +25,7 @@ class Listing(models.Model):
     lister = models.ForeignKey(User, on_delete=models.CASCADE, related_name="userListings")
     isClosed = models.BooleanField(default=False)
     
+    # Override __str__ method
     def __str__(self):
         return f"{self.id}. \n\
                 Title:\t{self.title},\n\
@@ -34,32 +33,38 @@ class Listing(models.Model):
                 Starting Bid: \t{self.startingBid},\n\
                 Image's URL: \t{self.imgURL},\n\
                 Category: \t{self.category}."
-    
-    
-# bids model
+
+# Bid model
 class Bid(models.Model):
+    # Define table's columns
     amount = models.DecimalField(max_digits=15, decimal_places=2)
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="userBids")
     auction = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listingBids")
     
+    # Define table's columns
     def __str__(self):
         return f"{self.id}. {self.bidder}: bidded {self.amount} on ({self.auction.id}. {self.auction.title})"
 
 
-# comments made on auction listings model
+# Comment(s) made on auction listings model
 class Comment(models.Model):
+    # Define table's columns
     text = models.CharField(max_length=1024)
     commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="userComments")
     auction = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listingComments")
     
+    # Override __str__ method
     def __str__(self):
         return f"{self.commenter}: {self.text}"
 
 
-# Watchlist
+# Watchlist model
 class Watchlist(models.Model):
+    # Define table's columns
     watcher = models.ForeignKey(User, on_delete=models.CASCADE, related_name="userWatchlist")
     auction = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listingWatchlists")
     
+    # Override __str__ method
     def __str__(self):
         return f"{self.id}. {self.watcher} is watching ({self.auction.id}. {self.auction.title})"
+
--- Project2Commerce/commerce/auctions/templatetags/util.py
+++ Project2Commerce/commerce/auctions/templatetags/util.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+# max filter
 @register.filter(name="max")
 def maxVal(value):
     return max(value)
